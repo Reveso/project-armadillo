973e7b23938d7baddf9bedbe2a7721482394236b
Reveso <lukas.rosz@gmail.com>
2018-11-14 23:48:44 +0100

    Inter-process communication prototype in Main, messenger.jar for testing it

--- src/testpackage/Main.java
+++ src/testpackage/Main.java
@@ -1,8 +1,29 @@
 package testpackage;
 
+import java.io.*;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("elo mordo");
+        Runtime rt = Runtime.getRuntime();
+//        String command = "java -jar F:\\Development\\messenger\\out\\artifacts\\messenger_jar\\messenger.jar";
+        String[] command = {"java", "-jar", "messenger.jar"};
+
+        try {
+            Process process = rt.exec(command);
+
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(process.getOutputStream()));
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            System.out.println(reader.readLine());
+
+            writer.write("Lukasz ma racje");
+            writer.flush();
+            writer.close();
+
+            System.out.println(reader.readLine());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 }
