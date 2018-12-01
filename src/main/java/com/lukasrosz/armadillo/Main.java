760ed79fd9785c62a340b6b6922102f9dff08506
Reveso <lukas.rosz@gmail.com>
2018-12-01 23:03:53 +0100

    sample + frontend templates

--- src/main/java/com/lukasrosz/armadillo/Main.java
+++ src/main/java/com/lukasrosz/armadillo/Main.java
@@ -1,19 +1,32 @@
 package com.lukasrosz.armadillo;
 
-import com.lukasrosz.armadillo.communication.ProcessCommunicator;
-import com.lukasrosz.armadillo.scoring.Score;
-import lombok.val;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-import java.io.*;
-import java.util.concurrent.*;
+public class Main extends Application {
 
-public class Main {
+//    public static void main(String[] args) {
+//        //TODO Get user input on what game does he want
+//        //TODO Create came by making GameMaker class instance, that will return the Scoreboard class
+////        System.out.println(System.getProperty("user.dir"));
+////        val gameMaker = new GameMaker();
+////        gameMaker.newBattleGame(new File(System.getProperty("user.dir")), 16);
+//    }
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        System.out.println(getClass());
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/sample.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
 
     public static void main(String[] args) {
-        //TODO Get user input on what game does he want
-        //TODO Create came by making GameMaker class instance, that will return the Scoreboard class
-//        System.out.println(System.getProperty("user.dir"));
-//        val gameMaker = new GameMaker();
-//        gameMaker.newBattleGame(new File(System.getProperty("user.dir")), 16);
+        launch(args);
     }
 }
---
+++ src/main/java/com/lukasrosz/armadillo/controller/MainWindowController.java
@@ -0,0 +1,4 @@
+package com.lukasrosz.armadillo.controller;
+
+public class MainWindowController {
+}
