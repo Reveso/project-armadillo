12427925871024af525b217a84a548170701c78e
soczystys <kmalinowski957@gmail.com>
2018-12-14 02:32:32 +0100

    Update fxml.java

--- src/main/java/fxml.java
+++ src/main/java/fxml.java
@@ -23,36 +23,23 @@ public class fxml {
 
     private FXMLLoader loader;
     //    FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/test.fxml"));
-    private BorderPane subPain;
-
-    public void method(ActionEvent actionEvent) throws IOException {
-            subPain = loader.load();
-//            subPain.
-        Stage stage = (Stage) mainPain.getScene().getWindow();
-//        stage.close();
-
-
-        mainPain.setRight(subPain);
-    }
+    private BorderPane subPain;git
 
     public void AIvsAI(ActionEvent actionEvent) throws IOException {
         loader = new FXMLLoader(getClass().getResource("/fxml/AIvsAI.fxml"));
         subPain = loader.load();
-        Stage stage = (Stage) mainPain.getScene().getWindow();
         mainPain.setRight(subPain);
     }
 
     public void callPvP(ActionEvent actionEvent) throws IOException {
         loader = new FXMLLoader(getClass().getResource("/fxml/PvP.fxml"));
         subPain = loader.load();
-        Stage stage = (Stage) mainPain.getScene().getWindow();
         mainPain.setRight(subPain);
     }
 
     public void callAIvsP(ActionEvent actionEvent) throws IOException {
         loader = new FXMLLoader(getClass().getResource("/fxml/AIvsP.fxml"));
         subPain = loader.load();
-        Stage stage = (Stage) mainPain.getScene().getWindow();
         mainPain.setRight(subPain);
     }
 }
