f481e48a388cd0dba74014b6b287f44063942b8d
soczystys <kmalinowski957@gmail.com>
2018-12-29 02:00:44 +0100

    one controller
    to rule them all

--- src/main/java/com/lukasrosz/armadillo/MainController.java
+++ src/main/java/com/lukasrosz/armadillo/MainController.java
@@ -1,20 +1,22 @@
 package com.lukasrosz.armadillo;
 
-import com.lukasrosz.armadillo.subcontrollers.Controller;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.control.Button;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.TextField;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
+import javafx.stage.DirectoryChooser;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ResourceBundle;
 
-public class MainController {
+public class MainController implements Initializable{
     @FXML
     public Button startButton;
     @FXML
@@ -24,36 +26,43 @@ public class MainController {
     @FXML
     public RadioButton PvPRadioButton;
     @FXML
+    public TextField sizeText;
+    @FXML
+    public TextField soloText1;
+    @FXML
+    public Button soloFileChooser1;
+    @FXML
+    public TextField soloText2;
+    @FXML
+    public Button soloFileChooser2;
+    @FXML
+    public CheckBox checkBox2;
+    @FXML
+    public BorderPane subPain;
+    @FXML
+    public Label boardsizeLabel;
+
+    @FXML
     private BorderPane mainPain;
 
-    private com.lukasrosz.armadillo.subcontrollers.AIvsPController aIvsPController;
-    private com.lukasrosz.armadillo.subcontrollers.bitwaController bitwaController;
-    private com.lukasrosz.armadillo.subcontrollers.PvPController pvPController;
-    private com.lukasrosz.armadillo.subcontrollers.soloController soloController;
-    private FXMLLoader loader;
-    private BorderPane subPain;
     private int size = 16;
     private int delay = 1;
 
-    public void loadPvP(ActionEvent actionEvent) throws IOException {
-        loader = new FXMLLoader(getClass().getResource("/fxml/gamemodecenter/replay.fxml"));
-        subPain = loader.load();
-        mainPain.setRight(subPain);
-        pvPController = loader.getController();
-    }
-
     public void loadSolo(ActionEvent actionEvent) throws IOException {
-        loader = new FXMLLoader(getClass().getResource("/fxml/gamemodecenter/solo.fxml"));
-        subPain = loader.load();
-        mainPain.setRight(subPain);
-        soloController = loader.getController();
+        setAllVisible(true);
     }
 
     public void loadBitwa(ActionEvent actionEvent) throws IOException {
-        loader = new FXMLLoader(getClass().getResource("/fxml/gamemodecenter/bitwa.fxml"));
-        subPain = loader.load();
-        mainPain.setRight(subPain);
-        bitwaController = loader.getController();
+        setAllVisible(false);
+        setOnlyFewVisible(true);
+    }
+
+    private void setOnlyFewVisible(boolean visible) {
+        clearFields();
+        sizeText.setVisible(visible);
+        soloText1.setVisible(visible);
+        soloFileChooser1.setVisible(visible);
+        boardsizeLabel.setVisible(visible);
     }
 
     public void onExitClicked(ActionEvent actionEvent) {
@@ -66,23 +75,19 @@ public class MainController {
         GameHandler gameHandler = new GameHandler();
 
         if (bitwaRadioButton.isSelected()) {
-            size = getSize(bitwaController);
-            File file = getFile(bitwaController.bitwText);
+            size = getSize();
+            File file = getFile(soloText1);
             gameHandler.startBattleGame(size, file, 100);
         } else if (soloRadioButton.isSelected()) {
-            size = getSize(soloController);
-            File file1 = getFile(soloController.soloText1);
-            File file2 = getFile(soloController.soloText2);
+            size = getSize();
+            File file1 = getFile(soloText1);
+            File file2 = getFile(soloText2);
             gameHandler.startSoloGame(file1, file2, size, 1);
-        } else if (PvPRadioButton.isSelected()) {
-            gameHandler.startPreviousGame(); //replay
         }
     }
 
-    private int getSize(Controller controller) {
-        int size;
-        size = Integer.parseInt(controller.sizeText.getCharacters().toString());
-        return size;
+    private int getSize() {
+        return Integer.parseInt(sizeText.getText());
     }
 
     private File getFile(TextField textField) {
@@ -90,13 +95,66 @@ public class MainController {
     }
 
     public void clearClicked(ActionEvent actionEvent) {
-        if (bitwaRadioButton.isSelected()) {
-            bitwaController.bitwText.clear();
-            bitwaController.sizeText.clear();
-        } else if (soloRadioButton.isSelected()) {
-            soloController.soloText1.clear();
-            soloController.soloText2.clear();
-            soloController.sizeText.clear();
+            clearFields();
+    }
+
+    private void clearFields() {
+        soloText1.clear();
+        soloText2.clear();
+        sizeText.clear();
+    }
+
+    public void soloFilechooser1Handler(ActionEvent actionEvent) {
+        DirectoryChooser dc = new DirectoryChooser();
+        Path path = Paths.get(System.getProperties().getProperty("user.dir"));
+        dc.setInitialDirectory(new File(String.valueOf(path)));
+        File selectedFile = dc.showDialog(null);
+
+        if (selectedFile != null) {
+            soloText1.clear();
+            soloText1.setText(selectedFile.getAbsolutePath());
+        } else {
+            System.err.println("wrong selected file");
         }
+    }
+
+    public void soloFileChooser2Handler(ActionEvent actionEvent) {
+        DirectoryChooser dc = new DirectoryChooser();
+        Path path = Paths.get(System.getProperties().getProperty("user.dir"));
+        dc.setInitialDirectory(new File(String.valueOf(path)));
+        File selectedFile = dc.showDialog(null);
+
+        if (selectedFile != null) {
+            soloText2.clear();
+            soloText2.setText(selectedFile.getAbsolutePath());
+        } else {
+            System.err.println("wrong selected file");
+        }
+    }
+
+    public void checkBox2selected(ActionEvent actionEvent) {
+        if (checkBox2.isSelected()) {
+            soloFileChooser2.setDisable(true);
+            soloText2.setDisable(true);
+            soloText2.clear();
+        } else {
+            soloFileChooser2.setDisable(false);
+            soloText2.setDisable(false);
+        }
+    }
+
+    private void setAllVisible(boolean visible) {
+        soloFileChooser1.setVisible(visible);
+        soloFileChooser2.setVisible(visible);
+        sizeText.setVisible(visible);
+        checkBox2.setVisible(visible);
+        soloText1.setVisible(visible);
+        soloText2.setVisible(visible);
+        boardsizeLabel.setVisible(visible);
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        setAllVisible(false);
     }
 }
